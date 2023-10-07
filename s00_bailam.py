c769604d8bbe3e5fe9fdb7ea5b224bf58a8f6967
khangyessir <146812226+khangyessir@users.noreply.github.com>
2023-10-07 08:01:05 +0000

    Nop bai cho anh Duy

--- s00_bailam.py
+++ s00_bailam.py
@@ -1,11 +1,25 @@
 """
 --- debai 
-Viet python ham hi()
-Tra ve cau chao 'Hi'
+Viet python ham hi(name)
+Tra ve cau chao 'Hi {name}'
 
 --- input 
-(ko co)
+hi(name)
 
---- output 
-Hi!
+--- output
+Hi {name}
+
+--- ví dụ 1
+hi('AI-BTX')
+
+output
+Hi AI-BTX
+
+--- ví dụ 2
+hi('HSU')
+
+output
+Hi HSU
 """
+def hi(name):
+  return f'Hi {name}'
